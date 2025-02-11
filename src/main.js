3583f3315e62a9f19e14a1f4331b7d1af1ba674e
Jaren Garman <jarengarman@protonmail.com>
2025-02-10 19:33:58 -0500

    refactor: Return an object from cleanData

--- src/main.js
+++ src/main.js
@@ -345,7 +345,11 @@ function savePoster() {
 }
 
 function cleanData() {
-  return unmotivationalPosters.map((posterData) => createPoster(posterData.img_url, posterData.name, posterData.description))
+  return unmotivationalPosters.reduce((posters, posterData) => {
+    const poster = createPoster(posterData.img_url, posterData.name, posterData.description)
+    posters[poster.id] = poster
+    return posters
+  }, {})
 }
 
 function unmotivationalSetup() {
